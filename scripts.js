d3ac40c9538fad328a5c9cf44549bea284f421cc
Steven Dytewski <40008730+Dy2ski@users.noreply.github.com>
2020-06-10 16:39:16 -0700

    got smoothe scroll to work for Contact Us

--- scripts.js
+++ scripts.js
@@ -10,6 +10,26 @@ function classToggle() {
   document.querySelector('.Navbar__Link-toggle')
     .addEventListener('click', classToggle);
 
+  function myFunction() {
+     document.getElementById("MessageSend").innerHTML = "Message Sent!";
+    }
+
+    // inspired by https://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+var TopOffset = 85;    
+
+$('a[data-smooth]').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top - TopOffset
+          }, 1000);
+          return false;
+        }
+      }
+    });
     
 
     
